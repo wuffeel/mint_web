5ddfcab6220dadc762a071c91f28c8312b6eede8
wuffeel <vbl06402@gmail.com>
2023-09-04 09:41:20 +0300

    Add specialist and user insertion database logic

--- lib/domain/service/abstract/user_service.dart
+++ lib/domain/service/abstract/user_service.dart
@@ -3,5 +3,7 @@ import '../../entity/user_model/user_model.dart';
 abstract class UserService {
   Future<UserModel?> getCurrentUser();
 
+  Future<UserModel> updateUserData(UserModel user);
+
   Future<void> logOut();
 }
