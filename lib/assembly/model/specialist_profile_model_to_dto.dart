18f7ad9ff131d9f238215bdb682f5308c7f25ae8
Vladyslav Blyzniuk <vbl06402@gmail.com>
2023-10-10 14:59:46 +0300

    Use only date for specialist profile model dto

--- lib/assembly/model/specialist_profile_model_to_dto.dart
+++ lib/assembly/model/specialist_profile_model_to_dto.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:mint_core/mint_assembly.dart';
+import 'package:mint_core/mint_utils.dart';
 
 import '../../data/model/specialist_profile_model_dto/specialist_profile_model_dto.dart';
 import '../../domain/entity/specialist_profile_model/specialist_profile_model.dart';
@@ -14,7 +15,7 @@ class SpecialistProfileModelToDto
         firstName: param.firstName,
         lastName: param.lastName,
         price: param.price,
-        experience: param.experience,
+        experience: DateTimeUtils.toUtcDateOnly(param.experience),
         specializations: param.specializations,
         about: param.about,
         education: param.education,
